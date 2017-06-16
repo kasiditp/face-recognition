04402a9133195ae31afdb14c6ec61d1ea4a55c28
Kasidit Phoncharoen <kasidit.p@ku.th>
2017-06-16 12:13:56 +0900

    pi cam

--- face_pi.py
+++ face_pi.py
@@ -1,42 +1,30 @@
-import face_recognition
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-
-# Load a sample picture and learn how to recognize it.
+import face_recognition
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
 obama_image = face_recognition.load_image_file("yamakawa.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image, num_jitters=1)[0]
+obama_face_encoding = face_recognition.face_encodings(obama_image, num_jitters=5)[0]
 
 jub_image = face_recognition.load_image_file("Kasidit2.jpg", mode='RGB')
-jub_face_encoding = face_recognition.face_encodings(jub_image, num_jitters=1)[0]
+jub_face_encoding = face_recognition.face_encodings(jub_image, num_jitters=5)[0]
 
 gua_image = face_recognition.load_image_file("Pakpon2.png", mode='RGB')
-gua_face_encoding = face_recognition.face_encodings(gua_image, num_jitters=1)[0]
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-
-    # Resize frame of video to 1/4 size for faster face recognition processing
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(small_frame)
@@ -80,12 +68,18 @@ while True:
                                                                      255), 1)
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    # cv2.imshow('Video', frame)
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+ 
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
